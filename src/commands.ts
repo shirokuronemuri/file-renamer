ef0dd36c494b7f6b374ed48695af95e1014412de
Ihor Pavliuk <akira.pavl@gmail.com>
2025-02-13 17:28:19 +0200

    added list command options

--- src/commands.ts
+++ src/commands.ts
@@ -4,13 +4,28 @@ import { getFileList, currentPath } from "./utils.js";
 
 export function processCommands() {
   yargs(hideBin(process.argv))
-    .command("list", "list the files in current directory", {}, async () => {
-      const fileList = await getFileList();
+    .command<{ absolute: boolean; path: string; }>("list", "list the files in current directory", (yargs) => {
+      yargs.option("absolute", {
+        alias: "a",
+        type: "boolean",
+        desc: "display absolute paths"
+      });
+      yargs.option("path", {
+        alias: "p",
+        type: "number",
+        default: currentPath
+      });
+    }, async (argv) => {
+      const fileList = await getFileList({
+        isAbsolute: argv.absolute,
+        directoryPath: argv.path
+      });
       for (let file of fileList) {
         process.stdout.write(file + "\n");
       }
     })
     .demandCommand()
+    .strict()
     .help()
     .parse();
 
