13f3ab57729090d8ee5d3b958d3647759f2630d2
Ihor Pavliuk <akira.pavl@gmail.com>
2025-02-13 12:28:43 +0200

    list command - added folder slashes and absolute path handling

--- src/commands.ts
+++ src/commands.ts
@@ -4,9 +4,11 @@ import { getFileList, currentPath } from "./utils.js";
 
 export function processCommands() {
   yargs(hideBin(process.argv))
-    .command("list", "list the files in current directory", {}, () => {
-      // console.log(getFileList());
-      console.log("hi");
+    .command("list", "list the files in current directory", {}, async () => {
+      const fileList = await getFileList();
+      for (let file of fileList) {
+        process.stdout.write(file + "\n");
+      }
     })
     .demandCommand()
     .help()
--- src/utils.ts
+++ src/utils.ts
@@ -1,12 +1,19 @@
+import { Dirent } from "node:fs";
 import fs from "node:fs/promises";
+import path from "node:path";
 
 export const currentPath = process.cwd();
 
 
 export async function getFileList({
-  path = currentPath,
+  directoryPath = currentPath,
   isAbsolute = false
-}: { path?: string; isAbsolute?: boolean; } = {}): Promise<string[]> {
-  const fileList = await fs.readdir(currentPath);
-  return fileList;
+}: { directoryPath?: string; isAbsolute?: boolean; } = {}): Promise<string[]> {
+  const fileList = await fs.readdir(directoryPath, { withFileTypes: true });
+  const formattedFileList: string[] = fileList.map(file => {
+    let formattedPath: string = isAbsolute ? path.resolve(file.name) : file.name;
+    formattedPath = formattedPath + (file.isDirectory() ? path.sep : "");
+    return formattedPath;
+  });
+  return formattedFileList;
 }
