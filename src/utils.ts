4e3a9c8c52f7e1b6d495a6f9e0b6b0e3ebfe5eb2
Ihor Pavliuk <akira.pavl@gmail.com>
2025-02-14 12:21:25 +0200

    fixed absolute path display

--- src/utils.ts
+++ src/utils.ts
@@ -4,15 +4,13 @@ import path from "node:path";
 
 export const currentPath = process.cwd();
 
-// TODO: fix -ap returning cwd instead of user input
-
 export async function getFileList({
   directoryPath = currentPath,
   isAbsolute = false
 }: { directoryPath?: string; isAbsolute?: boolean; } = {}): Promise<string[]> {
   const fileList = await fs.readdir(directoryPath, { withFileTypes: true });
   const formattedFileList: string[] = fileList.map(file => {
-    let formattedPath: string = isAbsolute ? path.resolve(file.name) : file.name;
+    let formattedPath: string = isAbsolute ? path.resolve(directoryPath, file.name) : file.name;
     formattedPath = formattedPath + (file.isDirectory() ? path.sep : "");
     return formattedPath;
   });
